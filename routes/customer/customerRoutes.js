a97f2df1b95206b05e72e1da15d4d91e85a52870
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2025-07-24 10:00:38 +0300

    restore

--- routes/customer/customerRoutes.js
+++ routes/customer/customerRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { createCustomer, activateCustomer, getCustomerDeposits, getCustomersByUnitId } = require('../../controller/customers/createCustomer.js');
 const { getAllCustomers } = require('../../controller/customers/getAllCustomers.js');
-const { editCustomer } = require('../../controller/customers/editCustomer.js');
+const { editCustomer, getUnitCustomers } = require('../../controller/customers/editCustomer.js');
 const { SearchCustomers, SearchCustomersByPhoneNumber, SearchCustomersByName } = require('../../controller/customers/searchCustomers.js');
 const checkAccess = require('../../middleware/roleVerify.js');
 const verifyToken = require('../../middleware/verifyToken.js');
@@ -44,6 +44,9 @@ router.get('/customer-details/:id',verifyToken, getCustomerDetails);
 
 router.post('/customer-statement',verifyToken, getCustomerStatement);
 router.post('/delete-customers',clearCustomerData)  
+
+router.get('/units/:id/customers', verifyToken, getUnitCustomers);
+
 
 
 
